45405cb283ae8e9ef772f3f251404ed2b3ea93f9
Matt Burke <spraints@gmail.com>
2014-01-18 16:45:06 -0500

    I can listen to a switch!!!!!!!!

---
+++ README.md
@@ -0,0 +1,5 @@
+This repository is a collection of things I did to make my kids think I knew what I was doing with a raspberry pi.
+
+### resources
+
+[Raspberry pi pinout](http://www.panu.it/raspberry/)
---
+++ talk.py
@@ -0,0 +1,32 @@
+# from http://raspberrypi.stackexchange.com/questions/9013/why-cant-i-turn-on-an-led-through-python
+
+from RPi import GPIO
+from time import sleep
+import atexit
+
+def main():
+  print "  2  4  6  8 10 12 14 16 18 20 22 24 26"
+  print "  1  3  5  7  9 11 13 15 17 19 21 23 25"
+
+  print " 1 = 3.3V"
+  print " 3 = input from switch (pull up)"
+  print " 6 = ground"
+  print "12 = output to LED"
+
+  #Use the physical pin numbers, not the logical names
+  GPIO.setmode(GPIO.BOARD)
+
+  GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  GPIO.setup(12, GPIO.OUT)
+
+  GPIO.add_event_detect(3, GPIO.RISING, callback=clicked, bouncetime=200)
+
+  while True:
+    sleep(60)
+
+def clicked(channel):
+  print "CLICK {}".format(channel)
+
+atexit.register(GPIO.cleanup)
+
+main()
