35242ba9df5f2fea0241be8e3f334bf4578c8208
Matt Burke <spraints@gmail.com>
2014-01-18 17:33:13 -0500

    Make blink self-describe its pins.

--- blink.py
+++ blink.py
@@ -1,14 +1,14 @@
 # from http://raspberrypi.stackexchange.com/questions/9013/why-cant-i-turn-on-an-led-through-python
 
-#  2  4  6  8 10 12 14 16 18 20 22 24 26
-#  1  3  5  7  9 11 13 15 17 19 21 23 25
-# Connect the LED to 6 and 12.
-
 from RPi import GPIO
 from time import sleep
 import atexit
 
 def main():
+  print "  2  4  6  8 10 12 14 16 18 20 22 24 26"
+  print "  1  3  5  7  9 11 13 15 17 19 21 23 25"
+  print " Connect the LED to 6 and 12."
+
   #Use the physical pin numbers, not the logical names
   GPIO.setmode(GPIO.BOARD)
 
