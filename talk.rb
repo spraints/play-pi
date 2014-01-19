9b86e8193eeac8f3ec1ab13ae907b0c683c6299a
Matt <spraints@matts-mac-mini.local>
2014-01-19 07:23:11 -0500

    Let me decide what to say when I run this.

--- talk.rb
+++ talk.rb
@@ -6,13 +6,15 @@ r,w = IO.pipe
 running = true
 [:INT, :TERM, :QUIT].each { |sig| trap(sig) { running = false ; w << '.' } }
 
+thing_to_say = ARGV
+thing_to_say = ['good day!'] if thing_to_say.empty?
+
 socket = UDPSocket.new
 socket.bind('', 4576)
 while running
   rs, ws = IO.select([socket, r])
   if rs.include?(socket)
     message, sender = socket.recvfrom(1024)
-    p [message, sender]
-    system "say", "hello hello hello i'm the one who loves you so"
+    system "say", *thing_to_say
   end
 end
